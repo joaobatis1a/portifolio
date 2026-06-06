9c3aafe9173e82fddaec761e4c1dcbd2b5c7f3c9
joaobatis1a <pessoalba1is1a@gmail.com>
2026-06-06 18:05:09 -0300

    feat(hero): h1 with TypeAnimation

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Contact from "./components/Contact";
 
 function App() {
     return(
-        <main className="bg-slate-950 tet-white">
+        <main className="bg-black text-white">
             <Hero />
             <About />
             <Skills />
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,27 @@
+import { TypeAnimation } from "react-type-animation";
+
 function Hero() {
     return (
         <section>
-            <div className="min-h-screen flex items-center justify-center">
-                <p className="relative rounded-full px-5 py-1 text-sm/9 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
-                    Seja bem-vindo(a)!
+            <div className="min-h-screen flex flex-col items-center justify-center gap-4">
+                <p className="relative rounded-full px-1 md:px-5 py-1 text-sm/4 md:text-sm/7 text-green-300 ring-1 ring-green/10">
+                    SEJA BEM-VINDO(A)!
                 </p>
+                <h1 className="text-1xl md:text-5xl font-bold bg-gradient-to-r bg-gradient-to-r from-emerald-400 via-green-400 to-cyan-400 bg-clip-text text-transparent">
+                    <TypeAnimation
+                        sequence={[
+                            "JOÃO BATISTA DA SILVA NETO",
+                            2000,
+                            "",
+                            500,
+                            "DESENVOLVEDOR FRONT-END",
+                            2000,
+                        ]}
+                        repeat={Infinity}
+                    />
+                </h1>
+
+
             </div>
         </section>
     );
